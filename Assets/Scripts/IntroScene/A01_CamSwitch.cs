ce9c3b26144b8ce092d6cf8ab2b79c8e39bb709e
Himanshu Chopra <himanshuchopra2008@outlook.com>
2020-12-30 19:37:20 +0530

    Added UI and Skybox

---
+++ Assets/Scripts/IntroScene/A01_CamSwitch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A01_CamSwitch : MonoBehaviour
+{
+    public GameObject firstCam, secondCam, credLeadDes;
+
+    void Start()
+    {
+        StartCoroutine(CamSwitcher());
+    }
+
+    IEnumerator CamSwitcher()
+    {
+        yield return new WaitForSeconds(3);
+        credLeadDes.SetActive(true);
+        yield return new WaitForSeconds(4);
+        secondCam.SetActive(true);
+        firstCam.SetActive(false);
+    }
+}
